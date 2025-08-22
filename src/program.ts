51ff767250fc58dd4754e5829e853e5032f2ef8f
Ziga Trop <ziga.trop@gmail.com>
2025-08-22 20:08:53 +0200

    ger

--- src/program.ts
+++ src/program.ts
@@ -1,13 +1,30 @@
 import "./toolbar.ts"
 import articleJson from "./json/program.json";
-import { animate, type AnimationParams } from "animejs";
+import { articleTransition, chemIcon1Transition, chemIcon2Transition, chipIcon1Transition } from "./program/animations.ts";
+import { animate } from "animejs";
 
 const articlesDiv = document.querySelector(".articles") as HTMLDivElement;
+
+let bgAnimationIndex = 0;
 
 const prevArticle = document.querySelector(".prev-article") as HTMLButtonElement;
 const nextArticle = document.querySelector(".next-article") as HTMLButtonElement;
 
-function switchArticle(next: boolean) {
+function switchBG(next: boolean) {
+	const articlesLen = articlesDiv.children.length;
+	if (next)
+		bgAnimationIndex = (bgAnimationIndex + 1) % articlesLen;
+	else
+		bgAnimationIndex = (bgAnimationIndex - 1 + articlesLen) % articlesLen;
+
+	chemIcon1Transition(bgAnimationIndex);
+	chemIcon2Transition(bgAnimationIndex);
+	chipIcon1Transition(bgAnimationIndex);
+}
+
+
+function switchArticle(next: boolean): void {
+	switchBG(next);
 	for (const article of articlesDiv.children) {
 		const oldIndex = parseInt(article.getAttribute("index")!);
 		const articlesLen = articlesDiv.children.length;
@@ -15,27 +32,16 @@ function switchArticle(next: boolean) {
 		const newIndex = next ? (oldIndex + 1) % articlesLen : (oldIndex - 1 + articlesLen) % articlesLen;
 
 		article.setAttribute("index", `${newIndex}`);
-
 		if (newIndex == 0) {
 			(article as HTMLElement).style.pointerEvents = "auto";
-			const animation = animate(article, {
-				duration: 1000,
-				ease: "linear",
-				autoplay: false,
-				translateX: next ? [-200, -150, -100, -50, 0] : [200, 150, 100, 50, 0],
-				opacity: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-			} as AnimationParams);
+			/* 			switchBackground(article.className, next); */
+
+			const animation = animate(article, articleTransition(1000, next, true, false));
 			animation.play();
 		}
 		else if (oldIndex == 0) {
 			(article as HTMLElement).style.pointerEvents = "none";
-			const animation = animate(article, {
-				duration: 600,
-				ease: "linear",
-				autoplay: false,
-				translateX: next ? [0, 50, 100, 150, 200] : [0, -50, -100, -150, -200],
-				opacity: [0.75, 0.5, 0.25, 0, 0],
-			} as AnimationParams);
+			const animation = animate(article, articleTransition(600, !next, false, true));
 			animation.play();
 		}
 	}
@@ -47,6 +53,7 @@ nextArticle.addEventListener("click", (_) => switchArticle(true));
 for (let i = 0; i < articleJson.length; i++) {
 	const article = document.createElement("article") as HTMLDivElement;
 	article.setAttribute("index", `${i}`);
+	article.className = `${articleJson[i].title.toLowerCase()}`
 
 	const dateDiv = document.createElement("div") as HTMLDivElement;
 	dateDiv.className = "date-div";
@@ -71,17 +78,27 @@ for (let i = 0; i < articleJson.length; i++) {
 	const buttonsDiv = document.createElement("div") as HTMLDivElement;
 	buttonsDiv.className = "article-buttons";
 
-	const readMore = document.createElement("button") as HTMLButtonElement;
-	readMore.type = "button";
-	readMore.className = "read-more";
-	readMore.innerText = "PREBERI VEČ";
+	if (article.getAttribute("index") != "1") {
+		const readMore = document.createElement("button") as HTMLButtonElement;
+		readMore.type = "button";
+		readMore.className = "read-more";
+		readMore.innerText = "PREBERI VEČ";
 
-	const enroll = document.createElement("button") as HTMLButtonElement;
-	enroll.type = "button";
-	enroll.className = "enroll";
-	enroll.innerText = "PRIJAVI SE";
+		readMore.addEventListener("click", (_) => {
+			/* SHOULDNT BE NEEDED, BUT THE ${json[i].type} didnt' work idk anymore fuck this
+			 *  */
+			const currArticle = document.querySelector("article[index='0']") as HTMLDivElement;
+			const redirect = currArticle.className.replaceAll(" ", "-");
+			document.location.href = `/pages/${redirect}.html`
+		});
 
-	buttonsDiv.append(enroll, readMore);
+		const enroll = document.createElement("button") as HTMLButtonElement;
+		enroll.type = "button";
+		enroll.className = "enroll";
+		enroll.innerText = "PRIJAVI SE";
+
+		buttonsDiv.append(enroll, readMore);
+	}
 
 	article.append(dateDiv, type, title, p, buttonsDiv);
 	articlesDiv.appendChild(article);
---
+++ src/program/animations.ts
@@ -0,0 +1,113 @@
+import { createAnimatable, type AnimationParams, } from "animejs";
+
+export const articleTransition = (duration: number, left: boolean, visible: boolean, fadeOut: boolean) => {
+	const translateArr = [200, 150, 100, 50, 0];
+	if (fadeOut)
+		translateArr.reverse();
+
+	return {
+		duration: duration,
+		ease: "linear",
+		autoplay: false,
+		translateX: left ? translateArr.map(e => e * -1) : translateArr,
+		opacity: visible ? [0, 1] : [1, 0]
+	} as AnimationParams;
+};
+
+/**
+ * Showing and hiding the chem icon on the interaktivne delavnice
+*/
+export const chemIcon1Animatable = createAnimatable(".chem-icon1", {
+	right: 1000,
+	top: 1000,
+	rotate: 1000,
+	ease: "linear"
+});
+
+export const chemIcon1Transition = (index: number) => {
+	switch (index) {
+		case 0: {
+			chemIcon1Animatable.right(-400);
+			chemIcon1Animatable.top(-100);
+			chemIcon1Animatable.rotate(0);
+			break;
+		}
+		case 1: {
+			chemIcon1Animatable.right(-1700);
+			chemIcon1Animatable.top(-100);
+			chemIcon1Animatable.rotate(60);
+			break;
+		}
+		case 2: {
+			chemIcon1Animatable.right(-400);
+			chemIcon1Animatable.top(900);
+			chemIcon1Animatable.rotate(-60);
+			break;
+		}
+	}
+}
+
+
+export const chemIcon2Animatable = createAnimatable(".chem-icon2", {
+	left: 1000,
+	top: 1000,
+	rotate: 1000,
+	ease: "linear"
+});
+
+export const chemIcon2Transition = (index: number) => {
+	switch (index) {
+		case 0: {
+			chemIcon2Animatable.left(-2000);
+			chemIcon2Animatable.top(-100);
+			chemIcon2Animatable.rotate(0);
+			break;
+		}
+		case 1: {
+			chemIcon2Animatable.left(-300);
+			chemIcon2Animatable.top(-100);
+			chemIcon2Animatable.rotate(60);
+			break;
+		}
+		case 2: {
+			chemIcon2Animatable.left(0);
+			chemIcon2Animatable.top(900);
+			chemIcon2Animatable.rotate(120);
+			break;
+		}
+	}
+}
+
+
+export const chipIcon1Animatable = createAnimatable(".chip-icon1", {
+	left: { duration: 1000, unit: "px" },
+	top: { duration: 1000, unit: "px" },
+	ease: "linear"
+});
+
+export const chipIcon1Transition = (index: number) => {
+	const chipIcon = document.querySelector(".chip-icon1") as HTMLElement;
+	if (!chipIcon || !chipIcon.parentElement) return;
+
+	const containerRect = chipIcon.parentElement.getBoundingClientRect();
+	const elemRect = chipIcon.getBoundingClientRect();
+
+	switch (index) {
+		case 0: {
+			chipIcon1Animatable.left(-275);
+			chipIcon1Animatable.top(-175);
+			break;
+		}
+		case 1: {
+			const rightPos = containerRect.width - elemRect.width + 275;
+			chipIcon1Animatable.left(rightPos);
+			break;
+		}
+		case 2: {
+			const centerHoriz = (containerRect.width / 2) - (elemRect.width / 2);
+			chipIcon1Animatable.left(centerHoriz);
+			chipIcon1Animatable.top(-50);
+			break;
+		}
+	}
+};
